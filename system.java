b2065218d8f09645d08e449065121bb26ac9c9c1
D'Artagnan Passailaigue <dartagnangilligan@gmail.com>
2023-05-12 16:55:20 -0400

    added the luhn algorithm to the rest of the program

--- system.java
+++ system.java
@@ -30,7 +30,7 @@ public class system {
         System.out.println("Enter your credit card number: ");
         String creditcard = input.next();
         if(creditcard.length() >= 9){
-            ;
+            luhnAlgo(creditcard);
         }
         else {
             System.out.println("Invalid credit card number.");
@@ -110,6 +110,25 @@ public class system {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    public static boolean luhnAlgo(String cardNo) {
+        int nDigits = cardNo.length();
+
+        int nSum = 0;
+        boolean isSecond = false;
+        for (int i = nDigits - 1; i >= 0; i--) {
+            int d = cardNo.charAt(i) - '0';
+
+            if (isSecond == true)
+                d = d * 2;
+
+            nSum += d / 10;
+            nSum += d % 10;
+
+            isSecond = !isSecond;
+        }
+        return (nSum % 10 == 0);
     }
 
     public static void main(String[] args){
